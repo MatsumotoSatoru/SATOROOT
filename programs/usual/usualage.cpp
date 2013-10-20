439d96379d01d14a9fc75e3a0b151694692964a3
Matsumoto Satoru <sc108050@gmail.com>
2013-10-20 13:50:30 +0900

    細かなコードの修正を行った

--- programs/usual/usualage.cpp
+++ programs/usual/usualage.cpp
@@ -1,4 +1,4 @@
 #include <iostream>
 void usualage(char *name,int i){
-  std::cout << name << " is " << i << "years old." << std::endl ;
+  std::cout << name << " is " << i << " years old." << std::endl ;
 }
